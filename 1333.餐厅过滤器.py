6dd18b20047ff5ab967ad5a0b1dc849eba89a134
kody <2442658787@qq.com>
2023-09-28 09:01:01 +0800

    2023年9月28日

--- 1333.餐厅过滤器.py
+++ 1333.餐厅过滤器.py
@@ -7,18 +7,9 @@
 # @lc code=start
 class Solution:
     def filterRestaurants(self, restaurants: List[List[int]], veganFriendly: int, maxPrice: int, maxDistance: int) -> List[int]:
-        i = 0
-        while i < len(restaurants):
-            if veganFriendly:
-                if restaurants[i][2]==0:
-                    del(restaurants[i])
-                    continue
-            if restaurants[i][3] > maxPrice or restaurants[i][4] > maxDistance:
-                del(restaurants[i])
-                continue
-            i += 1
-        restaurants.sort(key=lambda x:(x[1],x[0]),reverse=True)
-        return [rest[0] for rest in restaurants]
+        ans = [rest for rest in restaurants if rest[2]>=veganFriendly and rest[3]<=maxPrice and rest[4]<=maxDistance]
+        ans.sort(key=lambda x:(x[1],x[0]),reverse=True)
+        return [rest[0] for rest in ans]
             
 # @lc code=end
 
